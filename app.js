2a5c8b22b8e2a4104e3c7b588b781ac1a7db1cbc
Jack Dutton <duttonjack@Jacks-MacBook-Pro.local>
2023-11-29 14:13:43 -0500

    basic API request working

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
+// Front end project - requires cors api extension to work
+// Get basic $GET call going to 
+// Baisc API call working
+// Core Feature - get user input regarding calories and use that to display fruits 
 
+$.get("https://www.fruityvice.com/api/fruit/all", (data) => {
+  mainBlock(data)
+});
+
+function mainBlock(data){
+    console.log(data)
+    console.log(data[0].nutritions.calories)
+
+    for (element of data){
+        if (element.nutritions.calories < 70){
+            console.log(element)
+        }
+    }
+}
